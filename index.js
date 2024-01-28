d74edaa2767a22b36114647ce5865a538b3ab46a
Suliman Hakimi <122719578+SulimanHakimi@users.noreply.github.com>
2024-01-28 01:04:32 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 let textArea = document.getElementById("text");
 let writerName = document.getElementById("author");
 let btn = document.getElementById("new-quote");
-let authorName = document.getElementById("authorName");
 
 let i = 0;
 async function apiCall() {
@@ -19,6 +18,5 @@ btn.addEventListener("click", () => {
   apiCall().then((res) => {
     textArea.innerHTML = `<i class="fa fa-quote-left"></i> ${res.joke[i].joke}`;
     writerName.innerText = `- ${res.joke[i].writer}`;
-    authorName.innerText = ` by ${res.joke[i].author}`;
   });
 });
